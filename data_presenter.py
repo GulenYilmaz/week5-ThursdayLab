c23c773f8b08f98ac051c075a6c09707b29d5ba2
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2021-11-11 14:54:02 -0500

    done

--- data_presenter.py
+++ data_presenter.py
@@ -1,5 +1,5 @@
-#2 Open the CupcakeInvoices.csv.
-cupcake_invoices = open('./CupcakeInvoices.csv')
+
+cupcake_invoices = open("CupcakeInvoices.csv")  #2 Open the CupcakeInvoices.csv.
 
 #3 Loop through all the data and print each row.
 for row in cupcake_invoices :
@@ -11,10 +11,10 @@ for row in cupcake_invoices :
     print(row[2])
 
 #5 Loop through all the data and print out the total for each invoice (Note: this data is not provided by the csv, you will need to calculate it. Also, keep in mind the data from the csv comes back as a string, you will need to convert it to a float. Research how to do this.).
-    total +=(Int(row[4]*row[3]))
+    total +=(Int(row[3])*float(row[4]),2)
     print(total)
 #6  Loop through all the data, and print out the total for all invoices combined.
-    print(total)
+    print(total,2)
 #7  Close your open file.
-cupcake_invoices= close('./CupcakeInvoices.csv')
+cupcake_invoices.close()
 
